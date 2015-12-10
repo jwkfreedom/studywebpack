3e84e33523eef513e71f2dec2c5486329bc46645
jwkfreedom <weikun.jiang@gmail.com>
2015-12-10 22:37:30 +0800

    chapter2.2 ok

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var node_modules = path.resolve(__dirname, 'node_modules');
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
+
+config = {
+    entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/main.js')],
+    resolve: {
+        alias: {
+          'react': pathToReact
+        }
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    module: {
+        loaders: [{
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel', // 'babel-loader' is also a legal name to reference 
+          query: {
+            presets: ['react', 'es2015']
+          }
+        }],
+        noParse: [pathToReact]
+    }
+};
+
+module.exports = config;
